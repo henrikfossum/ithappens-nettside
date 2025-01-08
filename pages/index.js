2aad58db2424dfaee2ec3414381608639023db99
Henrik Fossum <henrik.molvik.fossum@gmail.com>
2025-01-08 12:40:51 +0100

    analytics

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import NavBar from '../components/NavBar';
 import Image from 'next/image';
 import ContactForm from '../components/ContactForm';
 import { useTheme } from '../context/ThemeContext';
-
+import { Analytics } from "@vercel/analytics/react";
 
 const ProjectMedia = ({ project, videoRef }) => {
   const isVideo = project.imageUrl.endsWith('.gif') || project.imageUrl.endsWith('.mp4');
